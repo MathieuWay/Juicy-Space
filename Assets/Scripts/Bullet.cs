39ded1a670776ae4ab9155aaeec6f9624c670adc
mathieuway91 <mathieu.way91@gmail.com>
2020-02-25 08:54:28 +0100

    player

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    private void FixedUpdate()
+    {
+        transform.Translate(transform.up * speed * Time.fixedDeltaTime);
+    }
+
+    private void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime < 0)
+            Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public float range;
+    public float offset;
+    public GameObject bulletPrefabs;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,36 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //GO RIGHT
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            float nextPos = transform.position.x + speed * Time.deltaTime;
+            if(nextPos < range/2)
+                transform.Translate(transform.right * speed * Time.deltaTime);
+            else if(transform.position.x > range / 2)
+            {
+                Vector3 pos = transform.position;
+                pos.x = range / 2;
+                transform.position = pos;
+            }
+        }
+        //GO LEFT
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            float nextPos = transform.position.x - speed * Time.deltaTime;
+            if (nextPos > -range / 2)
+                transform.Translate(-transform.right * speed * Time.deltaTime);
+            else if (transform.position.x < -range / 2)
+            {
+                Vector3 pos = transform.position;
+                pos.x = -range / 2;
+                transform.position = pos;
+            }
+        }
+        //Shoot
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletPrefabs, transform.position + transform.up * offset, Quaternion.identity);
+        }
     }
 }
