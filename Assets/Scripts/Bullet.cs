162319f6f0ded6de5d604ec4b2b559b38066e270
mathieuway91 <mathieu.way91@gmail.com>
2020-02-25 09:04:43 +0100

    Bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,9 +6,17 @@ public class Bullet : MonoBehaviour
 {
     public float speed;
     public float lifeTime;
+    public int direction;
+    public bool isPlayerShot;
     private void FixedUpdate()
     {
-        transform.Translate(transform.up * speed * Time.fixedDeltaTime);
+        Vector3 dir = Vector3.zero;
+        if (direction > 0)
+            dir = Vector3.up;
+        else
+            dir = Vector3.down;
+
+        transform.Translate(dir * speed * Time.fixedDeltaTime);
     }
 
     private void Update()
@@ -16,5 +24,18 @@ public class Bullet : MonoBehaviour
         lifeTime -= Time.deltaTime;
         if (lifeTime < 0)
             Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            //-1 life
+            Debug.Log("Une vie perdu");
+        }
+        else if(collision.gameObject.tag == "Invaders" && isPlayerShot)
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
